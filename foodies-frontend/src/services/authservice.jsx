54ff0685a4aee68102a84097202d04f343dc2004
poornimadevi22 <poornikutty02@gmail.com>
2024-03-27 11:30:39 +0530

    changes updated

--- foodies-frontend/src/services/authservice.jsx
+++ foodies-frontend/src/services/authservice.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import toast from "react-hot-toast";
 // import { useNavigate } from 'react-router-dom';
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'https://foodies-webpage.onrender.com';
 // const navigate = useNavigate();
 const authService = {
 
